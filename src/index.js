c3c0df43c034931c8f889d938427033663da31c5
mihailzuravlev <zimboroda@gmail.com>
2023-05-10 12:38:53 +0300

    change track to wav

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const audio = document.querySelector('.speedcontrolcontainer  audio');
+const playbackrate = document.querySelector('.speedcontrolcontainer input');
+const display = document.querySelector('.speedcontrolcontainer span');
+const displayvalue = val => {
+  return parseInt(val * 100, 10) + '%'
+}
+if (window.localStorage.pbspeed) {
+  audio.playbackRate = window.localStorage.pbspeed;
+  playbackrate.value = window.localStorage.pbspeed;
+}
+display.innerText = displayvalue(audio.playbackRate);
+playbackrate.addEventListener('change', e => {
+  audio.playbackRate = playbackrate.value;
+  display.innerText = displayvalue(playbackrate.value);
+  window.localStorage.pbspeed = playbackrate.value;
+});
+
+/*
 import {
   SuperpoweredWebAudio,
   SuperpoweredTrackLoader,
@@ -89,3 +107,4 @@ const demoApplication = new DemoApplication();
 window.startApp = demoApplication.startApp.bind(demoApplication);
 window.loadTrack = demoApplication.loadTrack.bind(demoApplication);
 window.onParamChange = demoApplication.onParamChange.bind(demoApplication);
+*/

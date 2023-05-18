a4cefae3f50e4e0502e6ffe9993158018c241b02
mihailzuravlev <zimboroda@gmail.com>
2023-05-18 16:00:28 +0300

    new track test

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ import {
   SuperpoweredGlue
 } from "../static/superpowered/SuperpoweredWebAudio.js";
 
-const trackName = 'SIG test-19 mix +12 db.mp3'
+const trackName = 'sig.mp3'
 
 const superPoweredWasmLocation = "/static/superpowered/superpowered.wasm";
 const playerProcessorUrl = "/static/processors/playerProcessor.js";
